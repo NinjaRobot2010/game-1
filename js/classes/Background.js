5021d05a796844e1f020577513fadddcf36d6658
Justin Schiller <schiller.justin@gmail.com>
2023-06-11 23:38:29 -0500

    Fixed Background class so that property names are consistant with other classes and image source is dynamic

--- js/classes/Background.js
+++ js/classes/Background.js
@@ -1,6 +1,6 @@
 class Background extends Sprite {
-   constructor(position = {x:0, y:0}, size = {w:0, h:0}, velocity) {
-     super({position}, size, './images/seamlessForest.jpg', {x: 0, y: 0}, {w: 1000, h: 1000});
+   constructor(position = {x:0, y:0}, size = {w:0, h:0}, imgSrc, velocity) {
+     super({position}, size, imgSrc, {x: 0, y: 0}, {w: 1000, h: 1000});
      this.velocity = velocity;
    }
  
